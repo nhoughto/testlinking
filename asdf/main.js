81dc75c373f4f569179e7c90261a7052467a314d
Sandbox Sync <support@getsandbox.com>
2015-08-02 04:36:37 +0000

    Changes

---
+++ asdf/main.js
@@ -0,0 +1,65 @@
+/**
+ * My API Sandbox
+ * 
+ */
+
+// A basic route returning a canned response
+Sandbox.define('/hello', 'get', function(req, res) {
+    // send 'Hello world' response
+    res.send('Hello world');
+});
+
+
+// Using stateful behaviour to simulate creating users
+Sandbox.define('/users', 'POST', function(req, res) {
+    // retrieve users or, if there are none, init to empty array
+    state.users = state.users || [];
+    
+    // persist user by adding to the state object
+    state.users.push(req.body);
+
+    return res.json({status: "ok"});
+});
+
+// Using stateful behaviour to simulate getting all users
+Sandbox.define('/users', 'GET', function(req, res) {
+    // retrieve users or, if there are none init, to empty array
+    state.users = state.users || [];
+
+    return res.json(state.users);
+});
+
+// Using named route parameters to simulate getting a specific user
+Sandbox.define('/users/{username}', 'GET', function(req, res) {
+    // retrieve users or, if there are none, init to empty array
+    state.users = state.users || [];
+
+    // route param {username} is available on req.params
+    var username = req.params.username;
+
+    // log it to the console
+    console.log("Getting user " + username + " details");
+
+    // use lodash to find the user in the array
+    var user = _.find(state.users, { "username": username});
+    
+    return res.json(user);
+});
+
+Sandbox.define('/asdf','GET', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    // Set the type of response, sets the Content-Type header.
+    res.type('application/json');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.json({
+        "status": "ok"
+    });
+})
--- stub/scripts/curl_json_booking.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-HTTP_VERB="$1"
-HTTP_DATA="$2"
-ROUTE="$3" || ""
-
-./curl_json.sh "http://aces.getsandbox.com/booking${ROUTE}" "$HTTP_VERB" "$HTTP_DATA"
