3261a790f9256193fd37cf907282783750358c99
Sandbox Sync <support@getsandbox.com>
2018-05-29 10:52:44 +0000

    Changes

--- subpath/main.js
+++ subpath/main.js
@@ -5,7 +5,7 @@
 // A basic route returning a canned response
 Sandbox.define('/hello', 'get', function(req, res) {
     // send 'Hello world' response
-    res.send('Hello world123456');
+    res.send('Hello world12345678');
 });
 
 
@@ -44,3 +44,22 @@ Sandbox.define('/users/{username}', 'GET', function(req, res) {
     
     return res.json(user);
 });
+
+
+Sandbox.define('/migrate','GET', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    // Set the type of response, sets the content type.
+    res.type('application/json');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.json({
+        "status": "ok"
+    });
+})
