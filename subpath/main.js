f77d5c40f575f2bf85885dc425470c1b3b267fb7
Sandbox Sync <support@getsandbox.com>
2019-04-02 01:14:49 +0000

    Changes

--- subpath/main.js
+++ subpath/main.js
@@ -1,5 +1,6 @@
-var chai = require('chai.js');
+var assert = require('chai.js').assert;
 
 Sandbox.define("/hey", function(req,res){
+  assert.equal(req.body, 'body value');
   res.send('hey2');
 })
