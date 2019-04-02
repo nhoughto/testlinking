1d434b97baaa22015d4e4a6518a99e75ea4fde33
Sandbox Sync <support@getsandbox.com>
2019-04-02 01:16:30 +0000

    Changes

--- subpath/main.js
+++ subpath/main.js
@@ -4,7 +4,7 @@ Sandbox.define("/hey", function(req,res){
     try {
         assert.equal(req.body, 'body value');
     } catch(e){
-        return res.send(400, e.message);
+        return res.json(400, {error:e.message});
     }
     res.send('hey2');
 })
