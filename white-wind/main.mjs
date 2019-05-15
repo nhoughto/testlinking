7bfeec416cec58b449135fb013b0a2e61857fb53
Sandbox Sync <support@getsandbox.com>
2019-05-15 23:40:19 +0000

    Changes

--- white-wind/main.mjs
+++ white-wind/main.mjs
@@ -5,7 +5,6 @@
 
 import { createUserHandler, getUsersHandler, getUserByUsernameHandler } from "users.mjs";
 
-
 // A basic route returning a canned response
 Sandbox.define('/hello3', 'get', function(req, res) {
     // send 'Hello world' response
