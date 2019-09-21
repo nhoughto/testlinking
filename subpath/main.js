17348246008e252b52caac63e31069c90ba28941
Sandbox Sync <support@getsandbox.com>
2019-09-21 09:46:31 +0000

    Changes

--- subpath/main.js
+++ subpath/main.js
@@ -1,6 +1,10 @@
-Sandbox.define("/hey", function(req,res) {
-    res.send('hey')
-    console.log("weird: " + JSON.stringify([
-        "level" => "25"
-    ]));
+var assert = require('chai.js').assert;
+
+Sandbox.define("/hey", function(req,res){
+    try {
+        assert.equal(req.query.value, 'correct');
+    } catch(e){
+        return res.json(400, {error:e.message});
+    }
+    res.send('success');
 })
