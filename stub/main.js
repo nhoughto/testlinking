08453deb5d23805b60e40d26a528b3510c73ddee
Sandbox Sync <support@getsandbox.com>
2020-07-16 21:51:05 +0000

    Changes

--- stub/main.js
+++ stub/main.js
@@ -33,7 +33,7 @@ Sandbox.define('/hello2/{q}', 'GET', function(req, res){
 })
  
 Sandbox.define('/hello', function(req, res){
-    res.status(200);
+    res.status(302);
     res.send('Not modified')
 })
 
